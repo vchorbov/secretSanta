48571f8ffdd9fef738df7fa504917ecc8d7f336f
Velichka Chorbova <vili.chorbova@gmail.com>
2021-03-17 11:25:23 +0100

    added instances that load the DBs from files

--- src/main/ServerMain.java
+++ src/main/ServerMain.java
@@ -7,13 +7,28 @@ import storage.StudentsWishesStorage;
 
 public class ServerMain {
     private static final int SERVER_PORT = 8888;
+    private static final String LOCATION_WISHES = "wishesDB";
+    private static final String LOCATION_CREDENTIALS = "credentialsDB";
 
     public static void main(String[] args) {
 
-          StudentsWishesStorage studentsWishesStorage = new StudentsWishesStorage();
-          StudentsCredentialsStorage studentsCredentialsStorage = new StudentsCredentialsStorage();
-          Vault vault = new Vault(studentsCredentialsStorage);
+        /*
+         * You can instantiate the classes StudentsWishesStorage and StudentsCredentialStorage
+         * without any parameters in the constructor. That way the app will start working with empty "DB"es.
+         *
+         * StudentsWishesStorage studentsWishesStorage = new StudentsWishesStorage();
+         * StudentsCredentialsStorage studentsCredentialsStorage = new StudentsCredentialsStorage();
+         *
+         */
 
-          new WishListServer(SERVER_PORT, vault,studentsWishesStorage).start();
+        /*
+        *  By instantiating the classes with the locations of the files where the info from the previous sessions is
+        *  being stored, we can pick up from where we left, creating a state.
+        */
+        StudentsWishesStorage studentsWishesStorage = new StudentsWishesStorage(LOCATION_WISHES);
+        StudentsCredentialsStorage studentsCredentialsStorage = new StudentsCredentialsStorage(LOCATION_CREDENTIALS);
+        Vault vault = new Vault(studentsCredentialsStorage);
+
+        new WishListServer(SERVER_PORT, vault, studentsWishesStorage).start();
     }
 }
